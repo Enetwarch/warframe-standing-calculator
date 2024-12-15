147556d3712b954be3f8e00f00629f3554aa6b2a
Molina <enetwarch@protonmail.com>
2024-12-15 13:51:05 +0800

    update

--- FactionSyndicates.java
+++ FactionSyndicates.java
@@ -1,5 +1,69 @@
 public class FactionSyndicates {
+
+    static String[] 
+    factionSyndicates = {
+        "Steel Meridian", 
+        "Arbiters of Hexis", 
+        "Cephalon Suda", 
+        "The Perrin Sequence", 
+        "Red Veil", 
+        "New Loka"
+    };
+    static double[][]
+    alliedOpposedEnemy = {
+        {1.00, 0, 0, -1.00, 0.50, -0.50},
+        {0, 1.00, 0.50, -0.50, -1.00, 0},
+        {0, .50, 1.00, 0, -0.50, -1.00},
+        {-1.00, -0.50, 0, 1.00, 0, 0.50},
+        {0.50, -1.00, -0.50, 0, 1.00, 0},
+        {-0.50, 0, -1.00, 0.50, 0, 1.00}
+    };
+
+    static void chooseFactionSyndicate() {
+        System.out.printf(
+            """
+            %s
+            Choose a faction syndicate
+            1. Steel Meridian
+            2. Arbiters of Hexis
+            3. Cephalon Suda
+            4. The Perrin Sequence
+            5. Red Veil
+            6. New Loka
+            %s
+
+            """
+            , Main.newPrintInstance
+            , Main.newPrintInstance
+        );
+    }
+
+    static void getUserInput() {
+        while (true) { 
+            System.out.printf(
+                "%s\nSelect the faction you chose: >> "
+                , Main.newInputInstance
+            );
+            try {
+                int userFaction = Main.scanner.nextInt();
+                if (userFaction >= 1 && userFaction <= 6) {
+                    break;
+                } else {
+                    Main.inputError();
+                }
+            } catch (Exception e) {
+                break;
+            }
+        }
+    }
+
+    static void 
+
     public static void main(String[] args) {
-        
+        while (true) {
+            chooseFactionSyndicate();
+            getUserInput();
+
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -8,52 +8,37 @@ public class Main {
     masteryRank = 0, 
     standingCap = 16000;
 
-    public static void mrInputError() {
+    static void inputError() {
         System.out.print(
-        """
-        Invalid input.
-        Please enter a valid mastery rank from 0 to 34.
-        Press enter to proceed.
-        """
-        );
-        scanner.nextLine();
-        scanner.nextLine();
-    }
+            """
+            Invalid input.
 
-    public static void uiInputError() {
-        System.out.print(
-        """
-        Invalid input.
-        Please enter a valid option from 0 to 12.
-        Press enter to proceed.
-        """
+            """
         );
-        scanner.nextLine();
-        scanner.nextLine();
     }
 
     static void welcomeMessage() {
         System.out.printf(
-        """
-        %s
-        Welcome to Warframe Standing Calculator! This is where it will display
-        the amount of days it takes to reach a particular rank, the grinding
-        needed for the materials needed, and the days your excess standing
-        materials will last for. Do note that this was made in November 2024,
-        which is before the Warframe 1999 update.
-        %s
+            """
+            %s
+            Welcome to Warframe Standing Calculator! This is where it will display
+            the amount of days it takes to reach a particular rank, the grinding
+            needed for the materials needed, and the days your excess standing
+            materials will last for. Do note that this was made in November 2024,
+            which is before the Warframe 1999 update.
+            %s
 
-        """
-        , newPrintInstance
-        , newPrintInstance
+            """
+            , newPrintInstance
+            , newPrintInstance
         );
     }
 
     static void getMasteryRank() {
         while (true) { 
             System.out.printf(
-            "%s\nPlease enter your master rank: >> "
-            , newInputInstance
+                "%s\nPlease enter your master rank (Accepts ranks 0 to 34): >> "
+                , newInputInstance
             );
             try {
                 masteryRank = scanner.nextInt(); 
@@ -61,10 +46,10 @@ public class Main {
                     System.out.print("\n");
                     break;
                 } else {
-                    mrInputError();
+                    InputError();
                 }
             } catch (Exception e) {
-                mrInputError();
+                InputError();
             }
         }
         standingCap += masteryRank * 500;
@@ -72,27 +57,27 @@ public class Main {
 
     static void syndicateOptions() {
         System.out.printf(
-        """
-        %s
-        Choose a syndicate
-        1. Faction Syndicates
-        2. Conclave
-        3. Cephalon Simaris
-        4. Ostron
-        5. The Quills
-        6. Solaris United
-        7. Vox Solaris
-        8. Ventkids
-        9. Entrati
-        10. Necraloid
-        11. The Holdfasts
-        12. Cavia
-        0. Exit
-        %s
+            """
+            %s
+            Choose a syndicate
+            1. Faction Syndicates
+            2. Conclave
+            3. Cephalon Simaris
+            4. Ostron
+            5. The Quills
+            6. Solaris United
+            7. Vox Solaris
+            8. Ventkids
+            9. Entrati
+            10. Necraloid
+            11. The Holdfasts
+            12. Cavia
+            0. Exit
+            %s
 
-        """
-        , newPrintInstance
-        , newPrintInstance
+            """
+            , newPrintInstance
+            , newPrintInstance
         );
     }
     
@@ -100,18 +85,18 @@ public class Main {
         int userInput;
         while (true) {
             System.out.printf(
-            "%s\nChoose the number of the syndicate: >> "
-            , newInputInstance
+                "%s\nChoose a syndicate (Enter a number from 0 to 12): >> "
+                , newInputInstance
             );
             try {
                 userInput = scanner.nextInt();
                 if (userInput >= 0 && userInput <= 12) {
                     break;
                 } else {
-                    uiInputError();
+                    InputError();
                 }
             } catch (Exception e) {
-                uiInputError();
+                InputError();
             }
         }
         return userInput;
