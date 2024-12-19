db9d0d5cc3459a522f073aa841aa2725d309fbe3
Molina <enetwarch@protonmail.com>
2024-12-20 00:17:45 +0800

    added comments for format

--- data/Format.java
+++ data/Format.java
@@ -4,146 +4,126 @@ import java.util.Scanner;
 public class Format {
 
     public static Scanner scanner = new Scanner(System.in);
+    // Scanner object to get the user input.
+    
+
+    /////////////////////////////////////////////////
 
 
-    //////////////////////////////
-
-
-    public static void inputError(
-        int min, 
-        int max
-        ) {
+    // Input methods
+    public static void inputError(int min, int max) {
         System.out.printf(
             """
             Invalid input. Only accepts numbers from %d to %d.
-            """
-            , min
-            , max
-        );
-    }
+            """, min, max
+        ); // Gets called by the getUserInput() method. 
+    } // Either the input is out of bounds or it caused an error.
     
-    public static int getUserInput(
-        String message, 
-        int min, 
-        int max
-        ) {
-        int userInput = -1;
+    public static int getUserInput(String message, int min, int max) {
+        int userInput = -1; // Temporary user input.
         while (true) {
             try {
-                System.out.printf(
-                    "%s: >> "
-                    , message
-                );
-                userInput = scanner.nextInt();
+                System.out.printf("%s: >> ", message); 
+                // Sends a message with this format: >> 
+                userInput = scanner.nextInt(); 
                 if (userInput >= min && userInput <= max) {
                     break;
                 } else {
-                    scanner.nextLine();
+                    scanner.nextLine(); // Absorbs the \n after the user presses enter.
                     inputError(min, max);
                 }
             } catch (Exception e) {
-                scanner.nextLine();
+                scanner.nextLine(); // Absorbs \n.
                 inputError(min, max);
             }
         }
-        scanner.nextLine();
+        scanner.nextLine(); // Absorbs \n.
         return userInput;
     }
 
     public static void inputBuffer() {
         System.out.print(
             "Press enter to proceed >> "
-        );
-        scanner.nextLine();
+        ); // Gives the user time to look at the output.
+        scanner.nextLine(); // Pauses till enter is pressed.
     }
 
 
-    //////////////////////////////////
-    
+    ////////////////////////////////////////////////////////////////
 
-    public static void pluralizeNames(
-        int[] amount, 
-        String[] name
-        ) {
+
+    // Pluralize methods
+    public static void pluralizeNames(int[] amount, String[] name) {
         for (int i = 0; i < name.length; i++) {
             if (amount[i] > 1) {
                 name[i] = name[i] + "s";
-            }
-        }
+            } // Pluralizes the element name if its amount is more than 1.
+        } // Used in sacrifice resources methods.
     }
     
     public static String pluralizeDays(int days) {
         if (days > 1) {
-            return "s";
+            return "s"; 
         } else {
-            return "";
-        }
-    }
+            return ""; 
+        } // Adds an "s" at the end of the day string if it is more than 1.
+    } // Keeps the day in singular form otherwise.
 
 
     ///////////////////////////////////////////////////////
 
-    
+
+    // Print methods
     public static void printArray(String[] list, int min) {
         for (int i = 0; i < list.length; i++) {
             System.out.printf(
                 """
                 %d. %s     
-                """
-                , i + min
-                , list[i]
-            );
-        }
+                """, i + min, list[i]
+            ); // Prints the given array with numbered bullets.
+        } // The number starts with the minimum value given.
     }
 
-    public static void printResourceArray(
-        int[] amount, 
-        String[] name
-        ) {
-        pluralizeNames(
-            amount,
-            name    
-        );
+    public static void printResourceArray(int[] amount, String[] name) {
+        pluralizeNames(amount,name);
         for (int i = 0; i < name.length; i++) {
             System.out.printf(
                 """
                 - %,d %s
-                """
-                , amount[i]
-                , name[i]
-            );
-        }
+                """, amount[i], name[i]
+            ); // Prints the given array with dash bullets.
+        } // Also prints the amount of the item.
     }
 
 
-    ///////////////////////////////////////
+    ///////////////////////////////////////////
 
 
+    // Exit methods
     public static void printSyndicateSelect() {
         System.out.print(
             """
             0. Return to syndicate select.     
-            """
+            """ // To exit the faction syndicate loop.
         );
     }
-
 
     public static void printExitProgram() {
         System.out.print(
             """
             0. Exit program     
-            """
+            """ // To exit the program.
         );
     }
-    
+
     public static void terminateProgram() {
         System.out.print(
             """
             Program terminated.
-            """
+            """ 
         );
-        scanner.close();
-        System.exit(0);
+        scanner.close(); // Closes the scanner to prevent resource leakage.
+        System.exit(0); // Terminates the program.
     }
 
 }
