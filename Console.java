efc46f039352228ae80ee552c88866994adea36c
Molina <enetwarch@protonmail.com>
2024-12-20 00:33:49 +0800

    added comments for console

--- Console.java
+++ Console.java
@@ -4,7 +4,7 @@ import data.Syndicates;
 
 public class Console {
 
-    static String[] syndicateOptions = {
+    private static String[] syndicateOptions = {
         "Faction Syndicates",
         "Conclave",
         "Cephalon Simaris",
@@ -20,72 +20,66 @@ public class Console {
     };
 
 
-    ////////////////////////////
+    ////////////////////////////////////
 
 
-    static void getSyndicate() {
-        Format.printArray(
-            syndicateOptions,
-            1    
-        );
-        Format.printExitProgram();
-        int userInput = Format.getUserInput(
-            "Choose a syndicate",
-            0,
-            syndicateOptions.length
-        );
-        switch (userInput) {
+    // Selection list for choosing what to calculate.
+    private static void getSyndicate() {
+        Format.printArray(syndicateOptions,1); // Prints the contents of syndicate options in a listed numbered form.
+        Format.printExitProgram(); // Prints out a 0. Exit Program as the last item in the list.
+        int userInput = Format.getUserInput("Choose a syndicate",0,syndicateOptions.length); // Asks user to choose a syndicate based on the number.
+        switch (userInput) { // Chooses the syndicate based on the user input.
             case 1: 
                 Faction.factionSyndicates();
-                break;
+                break; // Faction Syndicates uses Faction calculations.
             case 2: 
                 Syndicates.conclave.calculateToConsole();
-                break;
+                break; // Conclave syndicate uses Standard calculations.
             case 3: 
                 Syndicates.cephalonSimaris.calculateToConsole();
-                break;
+                break; // Cephalon Simaris syndicate uses OneRank calculations.
             case 4: 
                 Syndicates.ostron.calculateToConsole();
-                break;
+                break; // Ostron syndicate uses Standard calculations.
             case 5:
                 Syndicates.theQuills.calculateToConsole();
-                break;
+                break; // The Quills syndicate uses Standard calculations.
             case 6: 
                 Syndicates.solarisUnited.calculateToConsole();
-                break;
+                break; // Solaris United syndicate uses Standard calculations.
             case 7: 
                 Syndicates.voxSolaris.calculateToConsole();
-                break;
+                break; // Vox Solaris syndicate uses Standard calculations.
             case 8: 
                 Syndicates.ventkids.calculateToConsole();
-                break;
+                break; // Ventkids syndicate uses Standard calculations.
             case 9: 
                 Syndicates.entrati.calculateToConsole();
-                break;
+                break; // Entrati syndicate uses Standard calculations.
             case 10: 
                 Syndicates.necraloid.calculateToConsole();
-                break;
+                break; // Necraloid syndicate uses Standard calculations.
             case 11: 
                 Syndicates.theHoldfasts.calculateToConsole();
-                break;
+                break; // The Holdfasts syndicate uses Standard calculations.
             case 12:
                 Syndicates.cavia.calculateToConsole();
-                break;
+                break; // Cavia syndicate uses Standard calculations.
             case 0: 
-                Format.terminateProgram();
-                break;
-        }
+                Format.terminateProgram(); // Terminates program.
+        } // The break;s prevent the program from "falling" through the switch case structure.
     }
 
 
     ////////////////////////////////////////
 
 
+    // Main
     public static void main(String[] args) {
-        Syndicates.getMasteryRank();
-        Syndicates.makeSyndicates();
+        Syndicates.getMasteryRank(); // Gets the mastery rank of the user.
+        Syndicates.makeSyndicates(); // Initializes the objects to take into account the changes in mastery rank and standing cap.
         while (true) { 
-            getSyndicate();
+            getSyndicate(); // 
         }
     }
 
